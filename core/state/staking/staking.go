bce7b286f80b272b82d50451116b8bc5a79c623c
bin.lv <bin.lv@lightnet.io>
2021-03-11 14:10:15 +0800

    add statedb_caller for staking

---
+++ core/state/staking/staking.go
@@ -0,0 +1,33 @@
+package staking
+
+import (
+	"math/big"
+
+	"github.com/pkg/errors"
+
+	"github.com/ethereum/go-ethereum/common"
+)
+
+var (
+	// ErrEmptyValidatorSet returns when valset from smart-contract is empty
+	ErrEmptyValidatorSet = errors.New("empty validator set")
+	// ErrLengthOfCandidatesAndStakesMisMatch returns when lengths stakes and candidates are not match
+	ErrLengthOfCandidatesAndStakesMisMatch = errors.New("length of stakes is not equal to length of candidates")
+	// ErrLengthOfVotesAndStakesMisMatch returns when lengths voters and stakes are not match
+	ErrLengthOfVotesAndStakesMisMatch = errors.New("length of voters is not equal to length of stakes")
+
+	maxGasGetValSet uint64 = 500000000
+)
+
+type StakingCaller interface {
+	// GetValidators returns list of validators, calculate from current stateDB
+	GetValidators(common.Address) ([]common.Address, error)
+	// GetValidatorsData return information of validators including owner, totalStake and voterStakes
+	GetValidatorsData(common.Address, []common.Address) (map[common.Address]CandidateData, error)
+}
+
+type CandidateData struct {
+	Owner       common.Address
+	VoterStakes map[common.Address]*big.Int
+	TotalStake  *big.Int
+}
