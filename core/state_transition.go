aa67839d10f425ce049dc76886798639fe99c6ac
bin.lv <bin.lv@lightnet.io>
2021-03-18 15:42:55 +0800

    set miner not gain fees; set header coinbase is miner

--- core/state_transition.go
+++ core/state_transition.go
@@ -274,7 +274,7 @@ func (st *StateTransition) TransitionDb() (*ExecutionResult, error) {
 		ret, st.gas, vmerr = st.evm.Call(sender, st.to(), st.data, st.gas, st.value)
 	}
 	st.refundGas()
-	st.state.AddBalance(st.evm.Context.Coinbase, new(big.Int).Mul(new(big.Int).SetUint64(st.gasUsed()), st.gasPrice))
+	//st.state.AddBalance(st.evm.Context.Coinbase, new(big.Int).Mul(new(big.Int).SetUint64(st.gasUsed()), st.gasPrice))
 
 	return &ExecutionResult{
 		UsedGas:    st.gasUsed(),
